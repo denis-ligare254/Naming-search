cfdeeb45a4cd0724dcbe3c2c984d5d09f63d35de
denis <mrisholigare@gmail.com>
2021-02-04 12:26:23 +0300

    header and http imported

--- src/servives/profile.service.ts
+++ src/servives/profile.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {Http,Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username:string;
+  private clientid:"f3b887eca853e1857012";
+  private clientsecret:"d6a835cf0fd5bbd86a39fc873810711bdf85ffe8"
 
-  constructor() { }
+  constructor(private http:Http) {
+
+   }
 }
